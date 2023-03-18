7fe3bcd67b925bb3d810011af998fd840837f3e7
LoveRuHunter <113541018+LoveRuHunter@users.noreply.github.com>
2023-03-18 12:38:49 +0500

    update

---
+++ src/components/MovieGenre/MovieGenre.jsx
@@ -0,0 +1,39 @@
+import { Component } from "react";
+import { Typography } from "antd";
+
+const { Text } = Typography;
+
+import './MovieGenre.css';
+// eslint-disable-next-line import/no-unresolved
+import Context from "../context/context";
+
+export default class MovieGenre extends Component {
+    constructor(props) {
+        super(props);
+      }
+    
+      static contextType = Context;
+    
+      render() {
+        const { genre_ids } = this.props;
+    
+        const { genres } = this.context;
+        const genreNames = genres.map((element) => {
+          for (const genreId of genre_ids) {
+            if (element.id === genreId) {
+              return (
+                <Text
+                  code
+                  key={element.name}
+                  className="MovieGenre"
+                >
+                  {element.name}
+                </Text>
+              );
+            }
+          }
+        });
+    
+        return <div className="MovieGenresContainer">{genreNames}</div>;
+      }    
+}
